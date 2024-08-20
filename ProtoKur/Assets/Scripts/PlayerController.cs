99b53fd855d49cf36a1b11d37796942a546ac8a0
Alejandro <alejolr1218@gmail.com>
2024-08-20 10:51:16 -0500

    Basic Movement

--- ProtoKur/Assets/Scripts/PlayerController.cs
+++ ProtoKur/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,104 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Movement Variables")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCoolDown;
+    [SerializeField] private float airMultiplier;
+    [SerializeField] private float groundDrag;
+    [SerializeField] private Transform orientation;
+
+    private bool readyToJump = true;
+
+    [Header("Ground")]
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool grounded;
+
+    [Header("Keybinds")]
+    private KeyCode jumpKey = KeyCode.Space;
+
+
+    //Movement variables
+    private float horizontalinput;
+    private float verticalinput;
+
+    private Vector3 moveDirection;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
+    private void FixedUpdate(){
+        MovePlayer();
+    }
+
     void Update()
     {
-        
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        if(grounded){
+            rb.drag = groundDrag;
+        }
+        else{
+            rb.drag = 0;
+        }
+
+        Inputs();
+        SpeedControl();
+    }
+
+    private void Inputs(){
+        horizontalinput = Input.GetAxisRaw("Horizontal");
+        verticalinput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(jumpKey) && readyToJump && grounded){
+            Debug.Log("Salta");
+            
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCoolDown);
+        }
+    }
+
+    private void MovePlayer(){
+        moveDirection = orientation.forward * verticalinput + orientation.right * horizontalinput;
+
+        if(grounded){
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+
+        else if(!grounded){
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+
+    private void SpeedControl(){
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed){
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump(){
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump(){
+        readyToJump = true;
     }
 }
